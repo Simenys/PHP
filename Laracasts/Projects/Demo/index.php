9b4cb94a1d76e652ec453d3e75cf33e351f2d880
Simonas Zilevicius GP Office <s.zilevicius@gmail.com>
2025-03-25 15:46:46 +0100

    Continue learning

--- Laracasts/Projects/Demo/index.php
+++ Laracasts/Projects/Demo/index.php
@@ -4,8 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- CSS bellow -->
+    <style>
+        body {
+            display: grid;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+            font-family: 'Courier New', Courier, monospace;
+        }
+
+        ul > div {
+            display: flex;
+            align-items: center;
+            /* justify-content: center; */
+        }
+    </style>
 </head>
 <body>
-    <?php echo "Demo file works"; ?>
+    <h1>Recommended Books</h1>
+
+    <?php 
+        $books = [
+            [
+                'name' => 'Children Of Time',
+                'author' => 'Adrian Tchaikovsky',
+                'pages' => 657,
+                'link' => 'www.books.com',
+                'date' => 1989 
+            ],
+            [
+                'name' => 'Hail MAry',
+                'author' => 'Andy Weir',
+                'pages' => 523,
+                'link' => 'www.books.com',
+                'date' => 2023 
+            ],
+            [
+                'name' => 'Foundation',
+                'author' => 'Isaac Asimov',
+                'pages' => 320,
+                'link' => 'www.books.com',
+                'date' =>1978 
+            ]
+        ];
+    ?>
+
+    <div>
+        <ul>
+            <?php foreach ($books as $book) : ?>
+                <div>
+                    <a href="<?= $book['link'] ?>">
+                        <li><?= $book['name'] ?></li>
+                    </a>
+                    <p><?= " , " . $book['date'] ?></p>
+                </div>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+
 </body>
 </html>
