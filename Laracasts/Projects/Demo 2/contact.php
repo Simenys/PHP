5b96009d3471010823d893c7ba50af4ebf208c11
Simonas Zilevicius GP Office <s.zilevicius@gmail.com>
2025-03-28 15:35:26 +0100

    Continue

---
+++ Laracasts/Projects/Demo 2/contact.php
@@ -0,0 +1,5 @@
+<?php
+
+$heading = "Contacts";
+
+require 'views/contact.view.php';
---
+++ Laracasts/Projects/Demo 2/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$heading = "Home";
+
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+if ($_SERVER['REQUEST_URI'] === '/') {
+    echo 'bg-gray-900 text-white';
+} else {
+    echo 'text-gray-300';
+}
+
+require 'views/index.view.php';
